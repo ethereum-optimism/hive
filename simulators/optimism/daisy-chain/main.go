b50c03d514fea0562b72a5e1683f1603923512b4
clabby <ben@clab.by>
2023-03-21 01:51:53 -0400

    Add generic helper function to reduce duplication

--- simulators/optimism/daisy-chain/main.go
+++ simulators/optimism/daisy-chain/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"math/big"
 	"math/rand"
 	"time"
@@ -82,27 +83,48 @@ func runAllTests(tests []*optimism.TestSpec, fork string) func(t *hivesim.T) {
 	}
 }
 
-// Tests that a daisy-chained debug_traceBlockByNumber call to `op-geth` works as expected.
-func debugTraceBlockByNumberTest(t *hivesim.T, env *optimism.TestEnv) {
-	// Grab a random historical block
-	blockNumber := getRandomHistoricalBlockHex()
-
+// checkCallEquivalence is a helper function that checks that the result of an RPC call to
+// `op-geth` is equivalent to the result of a call to the historical sequencer.
+func checkCallEquivalence[K any](
+	t *hivesim.T,
+	env *optimism.TestEnv,
+	method string,
+	callback func(opGethRes K, histSeqRes K),
+	args ...interface{},
+) {
 	// Grab the result of the trace call from op-geth. This request should be daisy-chained.
 	opGeth := env.Devnet.GetOpL2Engine(0).RPC()
-	var opGethRes []*txTraceResult
-	err := opGeth.CallContext(env.Ctx(), &opGethRes, "debug_traceBlockByNumber", blockNumber)
-	require.NoError(t, err, "failed to call debug_traceBlockByNumber on op-geth")
+	var opGethRes K
+	err := opGeth.CallContext(env.Ctx(), &opGethRes, method, args)
+	require.NoError(t, err, fmt.Sprintf("failed to call %s on op-geth", method))
 
 	// Grab the result of the trace call from the historical sequencer endpoint. The result
 	// should be the same.
 	histSeq, err := rpc.DialHTTP(HistoricalSequencerRPC)
 	require.NoError(t, err, "failed to dial historical sequencer RPC")
-	var histSeqRes []*txTraceResult
-	err = histSeq.CallContext(env.Ctx(), &histSeqRes, "debug_traceBlockByNumber", blockNumber)
-	require.NoError(t, err, "failed to call debug_traceBlockByNumber on historical sequencer")
+	var histSeqRes K
+	err = histSeq.CallContext(env.Ctx(), &histSeqRes, method, args)
+	require.NoError(t, err, fmt.Sprintf("failed to call %s on historical sequencer", method))
 
-	// Compare the results.
-	require.Equal(t, histSeqRes, opGethRes, "results from historical sequencer and op-geth do not match")
+	// Perform the callback, which makes assertions about the equivalence of the two results.
+	callback(opGethRes, histSeqRes)
+}
+
+// Tests that a daisy-chained debug_traceBlockByNumber call to `op-geth` works as expected.
+func debugTraceBlockByNumberTest(t *hivesim.T, env *optimism.TestEnv) {
+	// Grab a random historical block
+	blockNumber := getRandomHistoricalBlockHex()
+
+	checkCallEquivalence(
+		t,
+		env,
+		"debug_traceBlockByNumber",
+		func(opGethRes []*txTraceResult, histSeqRes []*txTraceResult) {
+			// Compare the results
+			require.Equal(t, histSeqRes, opGethRes, "results from historical sequencer and op-geth do not match")
+		},
+		blockNumber,
+	)
 }
 
 // Tests that a daisy-chained debug_traceBlockByHash call to `op-geth` works as expected.
@@ -112,22 +134,16 @@ func debugTraceBlockByHashTest(t *hivesim.T, env *optimism.TestEnv) {
 	require.NoError(t, err, "failed to get block by number")
 	blockHash := block.Hash()
 
-	// Grab the result of the trace call from op-geth. This request should be daisy-chained.
-	opGeth := env.Devnet.GetOpL2Engine(0).RPC()
-	var opGethRes []*txTraceResult
-	err = opGeth.CallContext(env.Ctx(), &opGethRes, "debug_traceBlockByHash", blockHash.String())
-	require.NoError(t, err, "failed to call debug_traceBlockByHash on op-geth")
-
-	// Grab the result of the trace call from the historical sequencer endpoint. The result
-	// should be the same.
-	histSeq, err := rpc.DialHTTP(HistoricalSequencerRPC)
-	require.NoError(t, err, "failed to dial historical sequencer RPC")
-	var histSeqRes []*txTraceResult
-	err = histSeq.CallContext(env.Ctx(), &histSeqRes, "debug_traceBlockByHash", blockHash.String())
-	require.NoError(t, err, "failed to call debug_traceBlockByHash on historical sequencer")
-
-	// Compare the results.
-	require.Equal(t, histSeqRes, opGethRes, "results from historical sequencer and op-geth do not match")
+	checkCallEquivalence(
+		t,
+		env,
+		"debug_traceBlockByHash",
+		func(opGethRes []*txTraceResult, histSeqRes []*txTraceResult) {
+			// Compare the results
+			require.Equal(t, histSeqRes, opGethRes, "results from historical sequencer and op-geth do not match")
+		},
+		blockHash.String(),
+	)
 }
 
 // Tests that a daisy-chained debug_traceTransaction call to `op-geth` works as expected.
@@ -137,22 +153,16 @@ func debugTraceTransactionTest(t *hivesim.T, env *optimism.TestEnv) {
 	require.NoError(t, err, "failed to get block by number")
 	txHash := block.Transactions()[0].Hash()
 
-	// Grab the result of the trace call from op-geth. This request should be daisy-chained.
-	opGeth := env.Devnet.GetOpL2Engine(0).RPC()
-	var opGethRes *txTraceResult
-	err = opGeth.CallContext(env.Ctx(), &opGethRes, "debug_traceTransaction", txHash.String())
-	require.NoError(t, err, "failed to call debug_traceTransaction on op-geth")
-
-	// Grab the result of the trace call from the historical sequencer endpoint. The result
-	// should be the same.
-	histSeq, err := rpc.DialHTTP(HistoricalSequencerRPC)
-	require.NoError(t, err, "failed to dial historical sequencer RPC")
-	var histSeqRes *txTraceResult
-	err = histSeq.CallContext(env.Ctx(), &histSeqRes, "debug_traceTransaction", txHash.String())
-	require.NoError(t, err, "failed to call debug_traceTransaction on historical sequencer")
-
-	// Compare the results.
-	require.Equal(t, histSeqRes, opGethRes, "results from historical sequencer and op-geth do not match")
+	checkCallEquivalence(
+		t,
+		env,
+		"debug_traceTransaction",
+		func(opGethRes *txTraceResult, histSeqRes *txTraceResult) {
+			// Compare the results
+			require.Equal(t, histSeqRes, opGethRes, "results from historical sequencer and op-geth do not match")
+		},
+		txHash.String(),
+	)
 }
 
 // Tests that a daisy-chained debug_traceCall call to `op-geth` fails as expected.
@@ -160,7 +170,6 @@ func debugTraceCallTest(t *hivesim.T, env *optimism.TestEnv) {
 	// Grab the result of the trace call from op-geth. This request should be daisy-chained.
 	opGeth := env.Devnet.GetOpL2Engine(0).RPC()
 	err := opGeth.CallContext(env.Ctx(), nil, "debug_traceCall", make(map[string]interface{}), getRandomHistoricalBlockHex())
-
 	// The debug_traceCall method should not be implemented in op-geth's RPC.
 	require.Error(t, err, "debug_traceCall should not be implemented in op-geth")
 	require.Equal(t, err.Error(), "l2geth does not have a debug_traceCall method", "debug_traceCall should not be implemented in op-geth")
@@ -198,25 +207,19 @@ func ethCallTest(t *hivesim.T, env *optimism.TestEnv) {
 			Code: []byte{0x47, 0x60, 0x00, 0x52, 0x60, 0x20, 0x60, 0x00, 0xF3},
 		}
 
-	// Grab the result of the call from op-geth. This request should be daisy-chained.
-	opGeth := env.Devnet.GetOpL2Engine(0).RPC()
-	var opGethRes hexutil.Bytes
-	err := opGeth.CallContext(env.Ctx(), &opGethRes, "eth_call", tx, blockNumber, stateOverride)
-	require.NoError(t, err, "failed to call eth_call on op-geth")
-
-	// Grab the result of the call from the historical sequencer endpoint. The result
-	// should be the same.
-	histSeq, err := rpc.DialHTTP(HistoricalSequencerRPC)
-	require.NoError(t, err, "failed to dial historical sequencer RPC")
-	var histSeqRes hexutil.Bytes
-	err = histSeq.CallContext(env.Ctx(), &histSeqRes, "eth_call", tx, blockNumber, stateOverride)
-	require.NoError(t, err, "failed to call eth_call on historical sequencer")
-
-	// Compare the results.
-	// The expected result is 1e18 in hex (0x0de0b6b3a7640000).
-	expectedRes := hexutil.Bytes([]byte{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0xd, 0xe0, 0xb6, 0xb3, 0xa7, 0x64, 0x0, 0x0})
-	require.Equal(t, histSeqRes, expectedRes, "results from op-geth do not match")
-	require.Equal(t, histSeqRes, opGethRes, "results from historical sequencer and op-geth do not match")
+	checkCallEquivalence(
+		t,
+		env,
+		"eth_call",
+		func(opGethRes hexutil.Bytes, histSeqRes hexutil.Bytes) {
+			// Compare the results
+			// The expected result is 1 ether in hex (0xde0b6b3a7640000).
+			expectedRes := hexutil.Bytes([]byte{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0xd, 0xe0, 0xb6, 0xb3, 0xa7, 0x64, 0x0, 0x0})
+			require.Equal(t, histSeqRes, expectedRes, "results from op-geth do not match")
+			require.Equal(t, histSeqRes, opGethRes, "results from historical sequencer and op-geth do not match")
+		},
+		tx, blockNumber, stateOverride,
+	)
 }
 
 // Tests that a daisy-chained eth_estimateGas to `op-geth` works as expected.
@@ -228,23 +231,17 @@ func ethEstimateGasTest(t *hivesim.T, env *optimism.TestEnv) {
 	tx := types.NewTransaction(0, IDPrecompile, big.NewInt(0), 100_000, big.NewInt(0), payload)
 	blockNumber := getRandomHistoricalBlockHex()
 
-	// Grab the result of the call from op-geth. This request should be daisy-chained.
-	opGeth := env.Devnet.GetOpL2Engine(0).RPC()
-	var opGethRes hexutil.Uint64
-	err = opGeth.CallContext(env.Ctx(), &opGethRes, "eth_estimateGas", tx, blockNumber)
-	require.NoError(t, err, "failed to call eth_estimateGas on op-geth")
-
-	// Grab the result of the call from the historical sequencer endpoint. The result
-	// should be the same.
-	histSeq, err := rpc.DialHTTP(HistoricalSequencerRPC)
-	require.NoError(t, err, "failed to dial historical sequencer RPC")
-	var histSeqRes hexutil.Uint64
-	err = histSeq.CallContext(env.Ctx(), &histSeqRes, "eth_estimateGas", tx, blockNumber)
-	require.NoError(t, err, "failed to call eth_estimateGas on historical sequencer")
-
-	// Compare the results.
-	require.Greater(t, opGethRes, hexutil.Uint64(0), "gas estimate from op-geth should be greater than 0")
-	require.Equal(t, histSeqRes, opGethRes, "results from historical sequencer and op-geth do not match")
+	checkCallEquivalence(
+		t,
+		env,
+		"eth_estimateGas",
+		func(opGethRes hexutil.Bytes, histSeqRes hexutil.Bytes) {
+			// Compare the results.
+			require.Greater(t, opGethRes, hexutil.Uint64(0), "gas estimate from op-geth should be greater than 0")
+			require.Equal(t, histSeqRes, opGethRes, "results from historical sequencer and op-geth do not match")
+		},
+		tx, blockNumber,
+	)
 }
 
 // txTraceResult is the result of a single transaction trace.
